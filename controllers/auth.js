cec56b74a8a66a6f71d240b95f805af22fef927b
Soumajith Das <91211431+Soumajith@users.noreply.github.com>
2023-07-12 23:22:42 +0530

    Created controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,1 +1,42 @@
-const { user } = require("../models/user");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+exports.signup = async (request, response) => {
+  try {
+    const { name, email, password, role } = request.body;
+    const existingUser = await User.findOne({ email });
+
+    // check existing user
+    if (existingUser) {
+      return response.status(400).json({
+        success: false,
+        message: "Account already exist",
+      });
+    }
+
+    // created hashed password
+    let hashedPassword;
+    try {
+      hashedPassword = await bcrypt.hash(password, 10);
+    } catch (err) {
+      return response.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    }
+
+    // Create user object
+    const user = User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    // final resposne
+    return response.status(200).json({
+      success: true,
+      message: "Account successfully created.",
+    });
+  } catch (e) {}
+};
