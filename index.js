1104037812a768a92996abb3b0b073c2ac7469f6
Soumajith Das <91211431+Soumajith@users.noreply.github.com>
2023-07-12 22:43:35 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+require("dotenv").config();
+const dbconnect = require("./config/database");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+dbconnect();
+
+const user = require("./routes/user");
+app.use("/api/v1/", user);
+
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+// import controller
+const { login, signup } = require("../controllers/auth");
+
+//map
+router.post("/signup", signup);
+router.get("/login", login);
+
+//export
+module.exports = router;
